fd5b9bf3580ea2f12a614d9bc1aadb792bfe1071
AlTeveD2v <114943593+AlTeveD2v@users.noreply.github.com>
2022-12-05 16:20:09 +0300

    Add files via upload

---
+++ home_work_7-8_clustering/work.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+
+df = pd.read_csv("Credit Card Dataset.csv")
+
+df.isna().sum()
+# Отбросим пропуски
+df = df.dropna()
+
+# Применим boxplot
+sns.boxplot(df['BALANCE'], width=0.3, orient="h")
+plt.show()
+# Он ничего не показал
+
+# Применим кореллограмму
+plt.figure(dpi=80)
+sns.heatmap(df.corr(), xticklabels=df.corr().columns, yticklabels=df.corr().columns, cmap='RdYlGn', center=0, annot=True)
+plt.show()
+# Некоторые корреляции имеются
+
+# Метод локтя
+X = df.loc[:,['BALANCE', 'BALANCE_FREQUENCY', 'PURCHASES', 'ONEOFF_PURCHASES', 'INSTALLMENTS_PURCHASES', 'CASH_ADVANCE', 'PURCHASES_FREQUENCY', 'ONEOFF_PURCHASES_FREQUENCY', 'PURCHASES_INSTALLMENTS_FREQUENCY', 'CASH_ADVANCE_FREQUENCY', 'CASH_ADVANCE_TRX', 'PURCHASES_TRX', 'CREDIT_LIMIT', 'PAYMENTS', 'MINIMUM_PAYMENTS', 'PRC_FULL_PAYMENT', 'TENURE']]
+
+SSE = []
+
+for k in range(1, 9):
+    kmeans = KMeans(n_clusters = k)
+    kmeans.fit(X)
+    SSE.append(kmeans.inertia_)
+
+plt.plot(range(1,9), SSE, marker='s');
+plt.xlabel('k')
+plt.ylabel('SSE');
+plt.show()
+# Наибольший угол при k=2
+
+# Без предсказания
+plt.scatter(df['PAYMENTS'],df['PURCHASES'])
+plt.xlabel('PAYMENTS')
+plt.ylabel('PURCHASES')
+plt.show()
+
+# С предсказанием
+kmeans = KMeans(n_clusters = 2)
+kmeans.fit(X)
+Y_pred = kmeans.labels_
+plt.plot(X[Y_pred==1]['PAYMENTS'], X[Y_pred==1]['PURCHASES'], 'go', label='class2')
+plt.plot(X[Y_pred==0]['PAYMENTS'], X[Y_pred==0]['PURCHASES'], 'bo', label='class1')
+plt.xlabel('PAYMENTS')
+plt.ylabel('PURCHASES')
+plt.show()
